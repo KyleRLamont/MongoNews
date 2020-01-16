8e76ad51cac3fa1c1d23416510684e09137c0eb6
Kyle LaMont <kylerlamont@gmail.com>
2020-01-15 19:01:33 -0800

    updated title, attempted to fix notes not pulling from database, unable to dtermine where break occurred.

--- public/app.js
+++ public/app.js
@@ -10,7 +10,12 @@ var pageload = function () {
     }).then(function (data) {
         $("#articles").empty();
         for (var i = 0; i < data.length; i++) {
-            $("#articles").append("<p>" + data[i].title + "<br />" + "<a href ='https://www.nytimes.com" + data[i].link + "'>" + "Article Link" + "</a>" + "<br />" + "<button class='addnote' data-id='" + data[i]._id + "'>" + "Add Note" + "</button>" + "<button class='viewnote' data-id='" + data[i]._id + "'>" + "View Notes" + "</button>" + "</p>");
+            $("#articles").append("<p>" + data[i].title + 
+            "<br />" + 
+            "<a href ='https://www.nytimes.com" + data[i].link + "'>" + "Article Link" + "</a>" + 
+            "<br />" + 
+            "<button class='addnote' data-id='" + data[i]._id + "'>" + "Add Note" + "</button>" + 
+            "<button class='viewnote' data-id='" + data[i]._id + "'>" + "View Notes" + "</button>" + "</p>");
         }
     })
 }
@@ -40,28 +45,28 @@ $(document).on("click", ".addnote", function () {
 $(document).on("click", ".viewnote", function () {
     $("#notes").empty();
     var thisId = $(this).attr("data-id");
-    console.log(thisId)
+    
     $.ajax({
         method: "GET",
         url: "/articles/" + thisId
     }).then(function (data) {
-        console.log(data)
+        console.log(data.note)
         $("#notes").append("<p>" + data.note.title + "<br />" + data.note.body + "</p>")
     })
 })
 
 $(document).on("click", "#savenote", function () {
     var thisId = $(this).attr("data-id");
-
+    console.log(thisId)
     $.ajax({
         method: "POST",
         url: "/articles/" + thisId,
         data: {
-            title: $("#titleinput").val(),
-            body: $("#bodyinput").val()
+            title: $("#titleinput").val().trim(),
+            body: $("#bodyinput").val().trim()
         }
     }).then(function (data) {
-        console.log(data);
+        console.log(data.note.title);
         alert("Note Saved!");
         $("#notes").empty();
     });
